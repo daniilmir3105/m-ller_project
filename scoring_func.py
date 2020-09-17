0e6460e70639e1c83b91f7fa907d79d189de3c2f
daniilmir3105 <miroshnichienko.daniil@gmail.com>
2020-09-17 15:21:54 +0300

    add scoring_func.py

---
+++ scoring_func.py
@@ -0,0 +1,36 @@
+from abc import *
+from decimal import Decimal
+
+class base_class(metaclass=ABCMeta):
+    '''
+    This class will be base for our scoring class
+    '''
+
+    @abstractmethod
+    def rec(self, y, z):
+        return 108 - ((815 - 1500 / z) / y)
+
+class scoring_class(base_class):
+    '''
+    This is a class, that must score our results.
+    '''
+
+    def floatpt(self, N, func):
+        '''
+        This method will score our result in float datatype.
+        '''
+        
+        x = [4, 4.25]
+        for i in range(2, N + 1):
+            x.append(func(x[i - 1], x[i - 2]))
+        return x
+
+    def fixedpt(self, N, func):
+        '''
+        This method will score our result in decimal datatype.
+        '''
+
+        x = [Decimal(4), Decimal(17) / Decimal(4)]
+        for i in range(2, N + 1):
+            x.append(func(x[i - 1], x[i - 2]))
+        return x
